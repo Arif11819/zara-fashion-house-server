67e5f38023a7f6fe8b47c804a201421d9ade1c7a
Arif11819 <93979453+Arif11819@users.noreply.github.com>
2022-05-15 15:36:22 +0600

    added file after completing add new items

--- index.js
+++ index.js
@@ -32,6 +32,14 @@ async function run() {
             const query = { _id: ObjectId(id) };
             const items = await itemsCollection.findOne(query);
             res.send(items);
+        });
+
+        // Post
+        app.post('/items', async (req, res) => {
+            const newItems = req.body;
+            const result = await itemsCollection.insertOne(newItems);
+            res.send(result);
+
         })
     }
     finally {
